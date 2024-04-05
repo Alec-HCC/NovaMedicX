4294bb5e3df9b5b448af2bc9cdade11b527cd166
Fall <157811830+Alec-HCC@users.noreply.github.com>
2024-04-05 07:12:43 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,73 @@
+export const data = [
+    {
+      id: 1,
+      title: 'Mascarillas N95',
+      price: 10,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmeditechlaboratories.com%2Fproduct%2Fkn95-blanca-caja-x-50-unidades%2F&psig=AOvVaw25hjCURKsTXlmKB3y4733u&ust=1711421849193000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMCctq21joUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 2,
+      title: 'Guantes de látex',
+      price: 19,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vidri.com.sv%2Fproducto%2F97516%2Fguantes-de-latex-desechables-talla-l-100-pzas.html&psig=AOvVaw2AxEXbdoWT-pIgmsrL5ZyI&ust=1711421818093000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLiDi561joUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 3,
+      title: 'Termómetro digital',
+      price: 30,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FTerm%25C3%25B3metro-infrarrojos-sin-contacto-temperatura%2Fdp%2FB08NGR5QZR&psig=AOvVaw1Vo0zqhVBm9lWuBLrrbfAb&ust=1711421755470000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCprIC1joUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 4,
+      title: 'Alcohol en gel',
+      price: 12,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.walmart.com.sv%2Fdr-plus-alcohol-gel-500ml-1u%2Fp&psig=AOvVaw11vQZNNabBPeCxDE0g_Xiq&ust=1711421642795000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjDy8q0joUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 5,
+      title: 'Batas médicas',
+      price: 90,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.es%2Fvectores%2Fbata-medica&psig=AOvVaw2AWeIaUUzLTPokZdbHBZH3&ust=1711421595276000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiqk7S0joUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 6,
+      title: 'Medidor de azucar',
+      price: 100,
+      urlImage: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.medicalcanada.es%2Fimagenes%2FImage%2FProductos%2F018-1029_1_Medidor%2520Contour%2520018-1029.jpg&tbnid=SND4wuzs_Bqh5M&vet=12ahUKEwjoxr6fs46FAxVnJmIAHd9dBCAQMygHegQIARBk..i&imgrefurl=https%3A%2F%2Fwww.medicalcanada.es%2Faparato-medidor-de-glucosa-en-sangre-contour-018-1029&docid=xydwGZ2XOwUQrM&w=600&h=600&q=medidor%20de%20azucar&ved=2ahUKEwjoxr6fs46FAxVnJmIAHd9dBCAQMygHegQIARBk',
+      quantity: 1
+    },
+    {
+      id: 7,
+      title: 'Estetoscopio',
+      price: 25,
+      urlImage: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.bazarbiomedico.com%2Fwp-content%2Fuploads%2FDiseno-sin-titulo-9.png&tbnid=d77m6rtrc0hYdM&vet=12ahUKEwjD0d_Qs46FAxVpB2IAHUE_BCgQMygLegUIARCLAQ..i&imgrefurl=https%3A%2F%2Fwww.bazarbiomedico.com%2Fproducto%2Festetoscopio-littmann-classic-iii-edicion-black-smoke%2F&docid=GBiNLpBdEHAUGM&w=1080&h=1080&q=estetoscopio&ved=2ahUKEwjD0d_Qs46FAxVpB2IAHUE_BCgQMygLegUIARCLAQ',
+      quantity: 1
+    },
+    {
+      id: 8,
+      title: 'Silla de Ruedas',
+      price: 40,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.walmart.com.sv%2Fsilla-ruedas-drive-medical-silverrepos-pies-1-unidad%2Fp&psig=AOvVaw0hYC6qWa1SyIrJoFUCYa-r&ust=1711421418984000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC1-t-zjoUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 9,
+      title: 'Alcohol 90',
+      price: 30,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.walmart.com.sv%2Falcohol-con-yodo-lab-suizos-473ml%2Fp&psig=AOvVaw2FJ0tRDkYMFxAEXmVPTCq1&ust=1711421454285000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCICg4_CzjoUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    {
+      id: 10,
+      title: 'Bastones',
+      price: 39,
+      urlImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Finfrasalmedica.com%2Flineas-de-productos%2Fapoyo-y-movilidad%2F359-bastones-tipo-t&psig=AOvVaw0Hbq61RjKfZrFQGrfk9Jct&ust=1711421482623000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPiZrv6zjoUDFQAAAAAdAAAAABAE',
+      quantity: 1
+    },
+    
+  ];
